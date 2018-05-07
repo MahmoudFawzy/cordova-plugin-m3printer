a14a7bfb59a6c75001579aad4492ffe3916969f4
mahmoud <dev@itworkco.com>
2018-05-07 19:00:29 +0200

    1

--- www/printer.js
+++ www/printer.js
@@ -1,27 +1,25 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
 var printer = {
-    platforms: ['android'],
+    platforms: ["android"],
 
     isSupported: function() {
         if (window.device) {
             var platform = window.device.platform;
-            if ((platform !== undefined) && (platform !== null)) {
-                return (this.platforms.indexOf(platform.toLowerCase()) >= 0);
+            if (platform !== undefined && platform !== null) {
+                return this.platforms.indexOf(platform.toLowerCase()) >= 0;
             }
         }
         return false;
     },
     printText: function(text, onSuccess, onError) {
-        exec(onSuccess, onError, 'M3Printer', 'printText', [text]);
+        exec(onSuccess, onError, "M3Printer", "printText", [text]);
     },
     printBase64: function(base64, onSuccess, onError) {
-        alert(0)
-        exec(onSuccess, onError, 'M3Printer', 'printBase64', [base64]);
+        exec(onSuccess, onError, "M3Printer", "printBase64", [base64]);
     },
     printPath: function(base64, onSuccess, onError) {
-        alert(0)
-        exec(onSuccess, onError, 'M3Printer', 'printPath', [base64]);
+        exec(onSuccess, onError, "M3Printer", "printPath", [base64]);
     }
 };
 module.exports = printer;
